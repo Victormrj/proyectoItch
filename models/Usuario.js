894fc8117e767ecad78bfda7dc6f91e1c821c65e
Victor <vmrj090898@gmail.com>
2022-06-16 12:56:27 -0500

    Contraseña

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const { db } = require('../database/config');
 
-const Usuario = db.define('Usuario',{
+const Usuario = db.define('usuario',{
         nombre: {
             type: DataTypes.STRING,
             required: true
